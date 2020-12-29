c2bdb22a57df414e954a9e8c05bb366529fcd1e6
Gyeong <kamniang@naver.com>
2020-12-29 14:05:46 +0900

    save in external storate

--- app/src/main/java/com/osg/practicesignaturelibrary/MainActivity.kt
+++ app/src/main/java/com/osg/practicesignaturelibrary/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.osg.practicesignaturelibrary
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.ContextWrapper
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.AttributeSet
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -71,8 +75,8 @@ fun SignatureView() {
         }
         Button(onClick = {
             signature = signaturePad.signatureBitmap
-            val savePath = bitmapToFile(signature, context)
-            Log.d("file path",savePath.encodedPath.toString())
+            val savePath = saveImageInAlbum(context, "signature.jpg", signature)
+            Log.d("file path",savePath.toString())
         }) {
             Text("save")
         }
@@ -105,4 +109,49 @@ fun bitmapToFile(bitmap:Bitmap, context: Context): Uri {
 
     // Return the saved bitmap uri
     return Uri.parse(file.absolutePath)
+}
+
+fun saveImageInAlbum(context: Context, fileName: String, bitmap: Bitmap) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val values = ContentValues()
+        with(values) {
+            put(MediaStore.Images.Media.TITLE, fileName)
+            put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+            put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        }
+
+        val uri = context.getContentResolver()
+                .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val fos = context.contentResolver.openOutputStream(uri!!)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        fos?.run {
+            flush()
+            close()
+        }
+    } else {
+        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() +
+                File.separator
+        val file = File(dir)
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+
+        val imgFile = File(file, "test_capture.jpg")
+        val os = FileOutputStream(imgFile)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os)
+        os.flush()
+        os.close()
+
+        val values = ContentValues()
+        with(values) {
+            put(MediaStore.Images.Media.TITLE, fileName)
+            put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+            put(MediaStore.Images.Media.BUCKET_ID, fileName)
+            put(MediaStore.Images.Media.DATA, imgFile.absolutePath)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        }
+
+        context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    }
 }
